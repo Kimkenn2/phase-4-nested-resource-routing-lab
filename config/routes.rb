c5de2ae56e51562c523df67d55dbb01b34816c98
Kenneth Kim <kkm2059@gmail.com>
2022-01-12 11:25:54 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+  get '/users/:user_id/items', to: 'users#users_items'
+  get '/users/:user_id/items/:id', to: 'users#specific_item'
+  post '/users/:user_id/items', to: 'users#new_item'
 end
