de05317aed87cdb2d6550d6373a2608e02c7abee
Kenneth Kim <kkm2059@gmail.com>
2022-01-12 11:24:28 -0500

    commit tree c135c588b6fa24ed2c25f1a7cec02ce1cd35ac0f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,22 @@ class UsersController < ApplicationController
     render json: user, include: :items
   end
 
+  def users_items
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items
+  end
+
+  def specific_item
+    user = User.find(params[:user_id])
+    items = user.items
+    item = items.find(params[:id])
+    render json: item
+  end
+
+  def new_item
+    user = User.find(params:[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
 end
